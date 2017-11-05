38f8cbc9b23f1814821ce8d860ebd4e19f64b324
Aiswarya kodali <aishwarya.kodali@gmail.com>
2017-11-05 17:37:34 +1100

    Initial Commit

---
+++ ExtensionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExtensionDemo
+//
+//  Created by Aiswarya kodali on 5/11/17.
+//  Copyright © 2017 theSillyWayPossible. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
